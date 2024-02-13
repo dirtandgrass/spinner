042517a085f1f2842adfa282c3bef55254d91364
Luke <verdant.luke@gmail.com>
2024-02-12 20:36:19 -0500

    single iteration of spinner anim loop

---
+++ spinner1.js
@@ -0,0 +1,19 @@
+
+
+
+const animation = {
+  frames: ['|', '/', '-', '\\', '|', '/', '-', '\\'],
+  interval: 175,
+  index: 0,
+  intervalId: null,
+  start: function() {
+    for (let i = 0; i < this.frames.length; i++) {
+      setTimeout(() => {
+        process.stdout.write(`\r${this.frames[this.index]}  `);
+        this.index = (this.index + 1) % this.frames.length;
+      }, i * this.interval);
+    }
+  }
+};
+
+animation.start();
